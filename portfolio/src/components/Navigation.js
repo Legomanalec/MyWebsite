72a87df01d16c6f5fc964e6ae015bba80a6a9514
Legomanalec <alecrm4@iastate.edu>
2020-11-27 12:13:49 -0600

    Added portfolio react file

---
+++ portfolio/src/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Navigation.css";
+
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+       <div>
+          <span id="name">Alec R. Meyer</span>
+
+          <NavLink to="/">
+            <button id="home" class="button">
+              <span>Home</span>
+            </button>
+          </NavLink>
+
+          <NavLink id="about" to="/about">
+            <button id="about" class="button">
+              <span>About</span>
+            </button>
+          </NavLink>
+
+          <NavLink id="contact" to="/contact">
+            <button id="contact" class="button">
+              <span>Contact</span>
+            </button>
+          </NavLink>
+
+          <NavLink id="workExperience" to="/workExperience">
+            <button id="workExperience" class="button">
+              <span>Work Experience</span>
+            </button>
+          </NavLink>
+
+          <NavLink id="projects" to="/projects">
+            <button id="projects" class="button">
+              <span>Projects</span>
+            </button>
+          </NavLink>
+       </div>
+    );
+}
+
+export default Navigation;
